147b645b7319d2efa3cbc13759d1d694205fc5df
Danilo C. Graciano <danilo@compels.net>
2018-05-13 19:07:34 -0300

    commit initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  cars = [
+    { id: 1, name: 'Volvo' },
+    { id: 2, name: 'Saab', disabled: true },
+    { id: 3, name: 'Opel' },
+    { id: 4, name: 'Audi' },
+  ]
+
+}
